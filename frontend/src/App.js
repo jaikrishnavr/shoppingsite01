7ec52d837ad2e0a51591ff750e406a8c98253adc
jaikrishanvr <jaikrishnagokul@gmail.com>
2023-10-03 15:36:40 +0530

    Header(Navbar) & Footer Componenets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <h1 >welcome to shopit</h1>
-  )
+    <>
+      <Header/>
+        <main className="py-3">
+          <Container>
+            <h1>welcome to shopit</h1>
+          </Container>
+        </main>
+        <Footer/>
+    </>
+  );
 }
 
-export default App
+export default App;
