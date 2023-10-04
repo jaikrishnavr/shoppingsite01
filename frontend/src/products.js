6539d47c23647680a059543f9cc5af74a7f7e352
jaikrishanvr <jaikrishnagokul@gmail.com>
2023-10-04 13:44:05 +0530

    rating the product

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -14,7 +14,7 @@ const products = [
   },
   {
     _id: '2',
-    name: 'iPhone 13 Pro 256GB Memory',
+    name: 'iPhone 15 Pro 256GB Memory',
     image: '/images/phone.jpg',
     description:
       'Introducing the iPhone 13 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
@@ -22,8 +22,8 @@ const products = [
     category: 'Electronics',
     price: 599.99,
     countInStock: 7,
-    rating: 4.0,
-    numReviews: 8,
+    rating: 2,
+    numReviews: 4,
   },
   {
     _id: '3',
