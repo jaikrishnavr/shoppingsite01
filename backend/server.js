bbb55f4312f1839a7917c4ff1bf3d219c6efa9aa
jaikrishanvr <jaikrishnagokul@gmail.com>
2023-10-05 08:45:37 +0530

    (Backend) Express Server & Initial Product Routes

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import products from './data/products.js';
+
+const Port = 5000;
+
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send('Api is running...');
+});
+
+//to get all the products
+app.get('/api/products',(req,res) => {
+res.json(products);
+});
+
+//to get only one product
+app.get('/api/products/:id', (req,res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+    });
+
+app.listen(Port,()=> console.log(`server listening on port ${Port}`));
