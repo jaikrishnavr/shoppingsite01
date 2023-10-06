b418d3e0cb1dc6e234ef389e6f795a176b09b34e
jaikrishanvr <jaikrishnagokul@gmail.com>
2023-10-06 13:34:15 +0530

    connecting with MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ import express from 'express';
 import products from './data/products.js';
 
 import dotenv from 'dotenv';
+import connectDb from './config/db.js';
 dotenv.config();
 const Port = process.env.PORT || 5000;
 
+connectDb()
+
 const app = express();
+
+
 
 app.get('/',(req,res) => {
     res.send('Api is running...');
