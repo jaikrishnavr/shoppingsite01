c710952f51e552648dd459d6bfd29fcbc895d428
jaikrishanvr <jaikrishnagokul@gmail.com>
2023-10-11 12:06:19 +0530

    adding  Cart Item count in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,16 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Badge } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import logo from "../assets/logo.png";
 import { LinkContainer } from 'react-router-bootstrap'
+import { useSelector } from "react-redux";
 
 function Header() {
+
+  const { cartItems } = useSelector((state) => state.cart);
+
+  console.log(cartItems);
+
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -16,7 +23,14 @@ function Header() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <LinkContainer to='/Cart'>
-                <Nav.Link><FaShoppingCart /> Cart </Nav.Link>
+                <Nav.Link><FaShoppingCart /> Cart 
+                {
+                  cartItems.length > 0 && (
+                    <Badge pill bg='success' style={{marginLeft: '5px'}}>
+                      {cartItems.reduce((a,c) => a+c.qty, 0)}
+                    </Badge>
+                  )
+                }</Nav.Link>
               </LinkContainer>
               <LinkContainer to="/Login ">
                 <Nav.Link><FaUser /> Sign In</Nav.Link>
