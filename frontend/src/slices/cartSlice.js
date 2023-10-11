e05549a245bb81f7b1f1d5721a0b16f8535280b7
jaikrishanvr <jaikrishnagokul@gmail.com>
2023-10-11 11:52:14 +0530

    Cart Utils File

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,11 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../uitls/cartUtils";
 
 const initialState = localStorage.getItem("cart")
 ?JSON.parse(localStorage.getItem("cart")) 
 : {cartItems: []};  
-
-const addDecimals = (num) => { return (Math.round(num * 100)/100).toFixed(2);
-}
 
 
 
@@ -24,23 +22,7 @@ const cartSlice = createSlice({
                 state.cartItems = [...state.cartItems, item];
             }
 
-            //calculate items price
-            state.itemsPrice = addDecimals(state.cartItems.reduce((acc , item) => acc + item.price * item.qty, 0));
-
-            //calculate Shipping price(if the order is over $100 then it is free, else $10 shipping)
-             state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-
-            //calculate tax price (15% tax)
-              state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
-
-            //calculate total price
-               state.totalPrice = (
-                Number(state.itemsPrice) + 
-                Number(state.shippingPrice) +
-                Number(state.taxPrice)
-               ).toFixed(2);
-
-               localStorage.setItem('cart' , JSON.stringify(state));
+          return updateCart(state);
         },
     },
 });
