dacc92bd9c7683454ba3521be4ade3e2e278a235
jaikrishanvr <jaikrishnagokul@gmail.com>
2023-10-11 14:48:50 +0530

    remove from cart && proceed to checkout

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import Message from "../components/Message";
 import { FaTrash } from "react-icons/fa";
-import { addToCart } from "../slices/cartSlice";
+import { addToCart, removeFromCart } from "../slices/cartSlice";
 
 const CartScreen = () => {
   const navigate = useNavigate();
@@ -14,8 +14,17 @@ const CartScreen = () => {
   const { cartItems } = cart;
 
   const addToCartHandler = (product, qty) => {
-    dispatch(addToCart({...product, qty}))
-  }
+    dispatch(addToCart({...product, qty}));
+  };
+
+  const removeFromCartHandler = async (id) => {
+    dispatch(removeFromCart(id));
+  };
+
+
+  const checkoutHandler =() => {
+    navigate('/login?redirect=/shipping');
+  };
 
   return (
     <Row>
@@ -50,7 +59,7 @@ const CartScreen = () => {
                     </Form.Control>
                         </Col>
                         <Col md={2}>
-                            <Button type="button" variant='light'>
+                            <Button type="button" variant='light' onClick={( )=> removeFromCartHandler(item._id)}>
                                 <FaTrash />
                             </Button>
                         </Col>
@@ -71,7 +80,7 @@ const CartScreen = () => {
                     { cartItems.reduce((acc , item) => acc +item.qty * item.price, 0).toFixed(2)}
                 </ListGroup.Item>
                 <ListGroup.Item>
-                    <Button type="button" className="btn-block" disabled={cartItems.length === 0}>
+                    <Button type="button" className="btn-block" disabled={cartItems.length === 0} onClick={checkoutHandler}>
                         Proceed to checkout
                     </Button>
             </ListGroup.Item>
