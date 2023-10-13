223d36e21a75f09e2250b48d20ca80218f07a952
jaikrishanvr <jaikrishnagokul@gmail.com>
2023-10-13 15:38:05 +0530

    User profile endpoint -> getUserProfile -> updateUserProfile  (backend auth)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,7 +14,7 @@ const authUser = asyncHandler (async(req, res) => {
 if (user && (await user.matchPassword(password))) {
     generatetoken(res, user._id);
 
-    res.json({
+    res.status(200).json({
         _id: user._id,
         name:user.name,
         email:user.email,
@@ -79,7 +79,20 @@ const logoutUser = asyncHandler (async(req, res) => {
 //@route Get/api/users/profile
 //@access private  
 const getUserProfile = asyncHandler (async(req, res) => {
-    res.send('get user profile');
+ const user =  await User.findById(req.user._id);
+
+ if(user) {
+    res.status(200).json({
+            _id: user._id,
+            name:user.name,
+            email:user.email,
+            password:user.password,
+            isAdmin:user.isAdmin,
+    });
+ } else{
+    res.status(404);
+    throw new Error('User not found');
+ }
 });
 
 
@@ -87,7 +100,34 @@ const getUserProfile = asyncHandler (async(req, res) => {
 //@route Put/api/users/profile
 //@access private/Admin 
 const updateUserProfile = asyncHandler (async(req, res) => {
-    res.send('update user profile');
+    const user =  await User.findById(req.user._id);
+
+    if(user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        if (req.body.isAdmin) {
+            user.isAdmin = req.body.isAdmin;
+        }
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            password: updatedUser.password,
+            isAdmin: updatedUser.isAdmin,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+     }
+
 });
 
 
