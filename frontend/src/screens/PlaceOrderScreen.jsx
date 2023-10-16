948c5c6f9b16ab897b096d0cb76f91b9c5ffa2c1
jaikrishanvr <jaikrishnagokul@gmail.com>
2023-10-16 20:25:13 +0530

    Order Api Slice & order screen started (frontend checkout Process- 1 )

---
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import CheckoutSteps from '../components/CheckoutStep';
+import { Col, Row } from 'react-bootstrap';
+
+const PlaceOrderScreen = () => {
+
+  const navigate = useNavigate();
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    if (!cart.shippingAddress.address) {
+      navigate('/shipping');
+    } else if (!cart.paymentMethod) {
+      navigate('/payment');
+    }
+  }, [cart.paymentMethod, cart.shippingAddress, navigate]);
+
+  return (
+    <>
+      <CheckoutSteps step1 step2 step3 step4 />
+
+      <Row>
+        <Col md={8}>Column</Col>
+        <Col md={4}>Column</Col>
+      </Row>
+    </>
+    
+  );
+
+}
+
+export default PlaceOrderScreen
