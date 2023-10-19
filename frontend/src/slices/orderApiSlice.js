a108ec516eee7eb6507f42d45a63a2ea1fee9697
jaikrishanvr <jaikrishnagokul@gmail.com>
2023-10-19 18:24:05 +0530

    user profile / display order history

--- frontend/src/slices/orderApiSlice.js
+++ frontend/src/slices/orderApiSlice.js
@@ -2,37 +2,49 @@ import { apiSlice } from "./apiSlice";
 import { ORDERS_URL, PAYPAL_URL } from "../constants";
 
 export const orderApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        createOrder: builder.mutation({
-            query: (order) => ({
-                url: ORDERS_URL,
-                method: 'POST',
-                body: { ...order },
-            }),
-        }),
-        
-        getOrderDetails: builder.query({
-            query: (orderId) => ({
-              url: `${ORDERS_URL}/${orderId}`,
-            }),
-            keepUnusedDataFor: 5,
-          }),
+  endpoints: (builder) => ({
+    createOrder: builder.mutation({
+      query: (order) => ({
+        url: ORDERS_URL,
+        method: "POST",
+        body: { ...order },
+      }),
+    }),
 
-          payOrder: builder.mutation({
-            query: ({orderId, details}) => ({
-                url: `${ORDERS_URL}/${orderId}/pay`,
-                method: 'PUT',
-                body: { ...details },
-          }),
-        }), 
-getPayPalCLientId: builder.query({
-    query: () => ({
+    getOrderDetails: builder.query({
+      query: (orderId) => ({
+        url: `${ORDERS_URL}/${orderId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+
+    payOrder: builder.mutation({
+      query: ({ orderId, details }) => ({
+        url: `${ORDERS_URL}/${orderId}/pay`,
+        method: "PUT",
+        body: { ...details },
+      }),
+    }),
+    getPayPalCLientId: builder.query({
+      query: () => ({
         url: PAYPAL_URL,
+      }),
+      keepUnusedDataFor: 5,
     }),
-    keepUnusedDataFor: 5,
-})
 
-    }),
+    getMyOrders: builder.query({
+        query: () => ({
+            url: `${ORDERS_URL}/mine`,
+        }),
+        keepUnusedDataFor: 5,
+    })
+  }),
 });
 
-export const { useCreateOrderMutation , useGetOrderDetailsQuery, usePayOrderMutation, useGetPayPalCLientIdQuery} = orderApiSlice;
+export const {
+  useCreateOrderMutation,
+  useGetOrderDetailsQuery,
+  usePayOrderMutation,
+  useGetPayPalCLientIdQuery,
+  useGetMyOrdersQuery,
+} = orderApiSlice;
